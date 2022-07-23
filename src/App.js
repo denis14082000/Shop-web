32d50b56cbaf6340b5f4800414394153af66b0d4
Denis Kazantsev <denis.kazantsev@touchin.ru>
2022-07-23 21:10:29 +0300

    Added product page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from "./Product";
+import React from 'react';
+import {result} from "ramda/src/internal/_xfBase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            products: null
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/product', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        products: result.products
+                    });
+                },
+                (error) => {
+                    console.log(error);
+                }
+            )
+    }
+
+    render() {
+        const {isLoaded, products} = this.state;
+        if (!isLoaded) {
+            return <div>Загрузка...</div>;
+        } else {
+            return (
+                <div className="product-list">
+                    {
+                        products.map(product =>
+                            <Product product={product}/>
+                        )
+                    }
+                </div>
+            )
+        }
+    }
 }
 
 export default App;
